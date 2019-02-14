c44d3bf983247d65d77fb5dd97fcff8dddbda64d
Jura Hlotov <jglotovyn@gmail.com>
2019-02-15 00:16:41 +0200

    Commit

---
+++ AVR/charLCD/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include "lcd.h"
+
+FILE lcd_str = FDEV_SETUP_STREAM(lcd_putchar, NULL, _FDEV_SETUP_WRITE);
+int main(void)
+{
+    lcd_init();
+	stderr = &lcd_str;
+	fprintf(stderr, "AVR\n");
+
+	DDRB = 1;
+	while(1)
+    {
+        PORTB ^= 1;
+        _delay_ms(100);
+    }
+}
