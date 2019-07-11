24c8bbe790b205b5108968ffb834359530a58118
jura <jglotovyn@gmail.com>
2019-07-12 00:45:35 +0300

    Esp commands added

--- STM/stm32/meteo/module/esp/inc/Esp.h
+++ STM/stm32/meteo/module/esp/inc/Esp.h
@@ -1,6 +1,61 @@
 #ifndef _ESP_H
 #define _ESP_H
 
-void espinit(void);
+namespace Module{
+    class Esp{
+	
+
+	  private:
+		enum esp_work_state_t{
+		    ESP_SEND_CMD,
+		    ESP_RECEIVE_RESPONCE
+		};
+
+		enum esp_init_state_t{
+		    ESP_SUCCESS = 0,
+		    ESP_ERROR
+		};
+
+		enum at_command_t{
+		    AT,				  //test AT startup
+		    AT_RST,			  //restart
+		    AT_GMR,			  //check version info
+		    AT_GSLP,			  //enter deep sleep mode
+		    AT_E,			  //echo
+		    AT_RESTORE,			  //restore default settings
+		    AT_UART_CUR,		  //current configuration
+		    AT_CMD_NUMBER
+		};
+
+		enum at_responce_t{
+		    AT_OK,
+		    AT_RES_NUMBER
+		};
+
+		const char* atCommand[AT_CMD_NUMBER]    = {   "AT\r\n",
+							      "AT+RST\r\n",
+							      "AT+GMR\r\n",
+							      "AT+GSLP\r\n",
+							      "ATE\r\n",
+							      "AT+RESTORE\r\n",
+							      "AT+UART_CUR?\n\r"
+							  };
+		const char* atResponce[AT_RES_NUMBER]	= {   "OK",
+							      
+							  };
+
+		esp_work_state_t espState;
+		const char* getAtCommand(at_command_t);
+		void setEspState(esp_work_state_t);
+                esp_work_state_t getEspState(void);
+	public:
+	    
+	    Esp();
+	    virtual ~Esp();
+	    esp_init_state_t init(void);
+	    esp_init_state_t sendCommand(const char*);
+	    esp_init_state_t receiveResponce(char*);
+    };
+}
 
 #endif
--- STM/stm32/meteo/module/esp/src/Esp.cpp
+++ STM/stm32/meteo/module/esp/src/Esp.cpp
@@ -4,15 +4,41 @@
 #include <stdint.h>
 #include <stdio.h>
 
-static Driver::Uart *uart = new Driver::Uart();
+static Driver::Uart* uart = new Driver::Uart();
+static Module::Esp* esp = new Module::Esp();
 
-void espinit(void){
+//constructor
+Module::Esp::Esp(){
+
+}
+
+//destructor
+Module::Esp::~Esp(){
+
+}
+
+//init esp
+Module::Esp::esp_init_state_t Module::Esp::init(void){
 
     if(NULL == uart){
-	printf("Error. Can't create UART instance\n");
-	return;
+	printf("Esp.cpp. Can't create UART instance\n");
+	return ESP_ERROR;
+    }
+    if(NULL == esp){
+	printf("Esp.cpp. Can't create ESP instance\n");
+        return ESP_ERROR;
     }
 
-
+//uart init
     uart->init(Driver::Uart::UART_1, Driver::Uart::S_115200);
+
+    esp->setEspState(ESP_SEND_CMD);
+
+    esp->sendCommand(esp->getAtCommand(AT));
+    esp->sendCommand(esp->getAtCommand(AT_RESTORE));
+    esp->sendCommand(esp->getAtCommand(AT_GMR));
+    esp->sendCommand(esp->getAtCommand(AT_UART_CUR));
+    esp->sendCommand(esp->getAtCommand(AT_GMR));
+    esp->sendCommand(esp->getAtCommand(AT_GSLP));
+
 }
