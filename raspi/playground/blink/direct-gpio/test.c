492d7e3e5661fa3cff79124fbc590c1cd0a13072
Jura <jglotovyn@gmail.com>
2022-03-29 21:29:47 +0300

    led control over direct memory access

--- raspi/playground/blink/direct-gpio/test.c
+++ raspi/playground/blink/direct-gpio/test.c
@@ -3,26 +3,31 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
-#define BCM2708_PERI_BASE   0x20000000
-#define GPIO_BASE           (BCM2708_PERI_BASE + 0x200000) /* GPIO controller */
+#define BCM2711_PERI_BASE   0xfe000000
+#define GPIO_BASE           (BCM2711_PERI_BASE + 0x200000) /* GPIO controller */
 
-#define LED_18              18
+#define LED_PIN_26			7 /* physical pin 26; gpio 7 */
 
-#define BLOCK_SIZE (4*1024)
+#define BLOCK_SIZE			(4 * 1024)
+
+#define BLINK_TIMEOUT_US	200000	
 
 int mem_fd;
 void *gpio_map;
 volatile unsigned *gpio;
 
-// GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
+/**/
+static int init_io(void);
+
+/* GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y) */
 #define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
 #define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
 #define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
 
-#define GPIO_SET *(gpio+7)  // sets   bits which are 1 ignores bits which are 0
-#define GPIO_CLR *(gpio+10) // clears bits which are 1 ignores bits which are 0
+#define GPIO_SET *(gpio+7)  /* sets   bits which are 1 ignores bits which are 0 */
+#define GPIO_CLR *(gpio+10) /* clears bits which are 1 ignores bits which are 0 */
  
-int init_io()
+int init_io(void)
 {
     /* open /dev/mem */
     if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0)
@@ -36,12 +41,12 @@ int init_io()
         
         /* mmap GPIO */
         gpio_map = mmap(
-            NULL,             //Any adddress in our space will do
-            BLOCK_SIZE,       //Map length
-            PROT_READ|PROT_WRITE,// Enable reading & writting to mapped memory
-            MAP_SHARED,       //Shared with other processes
-            mem_fd,           //File to map
-            GPIO_BASE         //Offset to GPIO peripheral   
+            NULL,					/* Any adddress in our space will do */
+            BLOCK_SIZE,				/* Map length */
+            PROT_READ|PROT_WRITE,	/* Enable reading & writting to mapped memory */
+            MAP_SHARED,       		/* Shared with other processes */
+            mem_fd,           		/* File to map */
+            GPIO_BASE         		/* Offset to GPIO peripheral */   
         );
         
         close(mem_fd);
@@ -49,7 +54,7 @@ int init_io()
         
         if (gpio_map == MAP_FAILED) 
 		{
-            printf("mmap error %d\n", (int)gpio_map);//errno also set!
+            printf("mmap error %d\n", (int)gpio_map);
             return(-1);
         }
 		else
@@ -58,14 +63,14 @@ int init_io()
         }
     }
     
-    // Always use volatile pointer!
-   gpio = (volatile unsigned *)gpio_map;
+	/* Always use volatile pointer! */
+	gpio = (volatile unsigned *)gpio_map;
 
-   return 0;
+	return 0;
 }
 
-
-int main()
+/**/
+int main(void)
 {    
     if(init_io() == -1) 
     {
@@ -73,19 +78,23 @@ int main()
         return -1;
     }
     
-    // must use INP_GPIO before we can use OUT_GPIO
-    INP_GPIO(LED_18); 
-    OUT_GPIO(LED_18);
+    /* must use INP_GPIO before we can use OUT_GPIO */
+	INP_GPIO(LED_PIN_26); 
+    OUT_GPIO(LED_PIN_26);
     
-    for (int i = 0; i < 5; i++)
+    for (;;)
     {
-        GPIO_CLR = 1 << LED_18;
+        GPIO_CLR = 1 << LED_PIN_26;
+#if 0
         printf("LED OFF\n");
-        sleep(2);
+#endif        
+		usleep(BLINK_TIMEOUT_US);
 
-        GPIO_SET = 1 << LED_18;
+        GPIO_SET = 1 << LED_PIN_26;
+#if 0
         printf("LED ON\n");
-        sleep(1);
+#endif        
+		usleep(BLINK_TIMEOUT_US);
     }
     
     printf("- finished -\n");
