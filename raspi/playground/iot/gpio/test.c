191804f823e172a3168baa36c33b7482839047e7
JHlotov <jglotovyn@gmail.com>
2022-09-15 14:56:06 +0100

    bcm library test

--- raspi/playground/iot/gpio/test.c
+++ raspi/playground/iot/gpio/test.c
@@ -1,6 +1,8 @@
 #include <bcm2835.h>
 #include <stdio.h>
 
+#define LED_PIN RPI_BPLUS_GPIO_J8_37
+
 int main(int argc, char** argv)
 {
 	if (!bcm2835_init())
@@ -9,14 +11,14 @@ int main(int argc, char** argv)
 		return 1;
 	}
 	
-	bcm2835_gpio_fsel(RPI_BPLUS_GPIO_J8_07, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(LED_PIN, BCM2835_GPIO_FSEL_OUTP);
 
 	while (1)
 	{
-		bcm2835_gpio_write(RPI_BPLUS_GPIO_J8_07, HIGH);
-		bcm2835_delay(500);
-		bcm2835_gpio_write(RPI_BPLUS_GPIO_J8_07, LOW);
-		bcm2835_delay(500);
+		bcm2835_gpio_write(LED_PIN, HIGH);
+		bcm2835_delay(100);
+		bcm2835_gpio_write(LED_PIN, LOW);
+		bcm2835_delay(100);
 	}
 
 	bcm2835_close();
