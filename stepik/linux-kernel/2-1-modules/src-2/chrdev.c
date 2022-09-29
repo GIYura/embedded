f8b29254b3cddf9337bc0dbef82da50948175fec
JHlotov <jglotovyn@gmail.com>
2022-09-29 13:25:47 +0100

    update kernel module

--- stepik/linux-kernel/2-1-modules/src-2/chrdev.c
+++ stepik/linux-kernel/2-1-modules/src-2/chrdev.c
@@ -5,7 +5,6 @@
 #include <linux/uaccess.h>
 #include <linux/slab.h>
 
-static char* kbuf;
 static dev_t first;
 static unsigned int count = 1;
 static int dev_major = 700;
@@ -18,26 +17,61 @@ static struct cdev* my_cdev;
 
 static int mychrdev_open(struct inode* inode, struct file* file)
 {
+	static int counter = 0;
+
+	char* kbuf = kmalloc(KBUF_SIZE, GFP_KERNEL);
+
+	file->private_data = kbuf;
+
 	printk(KERN_INFO "Open %s\n", MYDEV_NAME);
+	
+	counter++;
+
+	printk(KERN_INFO "counter: %d\n", counter);
+	printk(KERN_INFO "module refcounter: %d\n", module_refcount(THIS_MODULE));
+
 	return 0;
 }
 
 static int mychrdev_release(struct inode* inode, struct file* file)
 {
 	printk(KERN_INFO "Release %s\n", MYDEV_NAME);
+
+	char* kbuf = file->private_data;
+
+	printk(KERN_INFO "Free\n");
+
+	if (kbuf)
+	{
+		kfree(kbuf);
+	}
+	kbuf = NULL;
+	file->private_data = NULL;
 	return 0;
 }
 
 static ssize_t mychrdev_read(struct file* file, char __user* buf, size_t lbuf, loff_t* pos)
 {
-	printk(KERN_INFO "Read %s\n", MYDEV_NAME);
-	return 0;
+	char* kbuf = file->private_data;
+
+	int nbytes = lbuf - copy_to_user(buf, kbuf + *pos, lbuf);
+	*pos += nbytes;
+
+	printk(KERN_INFO "Read %s nbytes = %d pos = %d\n", MYDEV_NAME, nbytes, (int)*pos);
+
+	return nbytes;
 }	
 
 static ssize_t mychrdev_write(struct file* file, const char __user* buf, size_t lbuf, loff_t* pos)
 {
-	printk(KERN_INFO "Write %s\n", MYDEV_NAME);
-	return 0;
+	char* kbuf = file->private_data;
+
+	int nbytes = lbuf - copy_from_user(kbuf + *pos, buf, lbuf);
+	*pos += nbytes;
+
+	printk(KERN_INFO "Write %s nbytes = %d pos = %d\n", MYDEV_NAME, nbytes, (int)*pos);
+
+	return nbytes;
 }
 
 static const struct file_operations mycdev_fops = 
@@ -52,8 +86,6 @@ static const struct file_operations mycdev_fops =
 static int __init init_chrdev(void)
 {
 	printk(KERN_INFO "Hello Kernel\n");
-
-	kbuf = kmalloc(KBUF_SIZE, GFP_KERNEL);
 
 	first = MKDEV(dev_major, dev_minor);
 	register_chrdev_region(first, count, MYDEV_NAME);
@@ -70,6 +102,12 @@ static int __init init_chrdev(void)
 static void __exit cleanup_chrdev(void)
 {
 	printk(KERN_INFO "Module removed\n");
+	if (my_cdev)
+	{
+		cdev_del(my_cdev);
+	}
+	unregister_chrdev_region(first, count);
+
 }
 
 module_init(init_chrdev);
