c3ab0c8822c2e2a0f3fe9fcd3dc1f4442451ede5
JHlotov <jglotovyn@gmail.com>
2023-01-16 10:01:55 +0000

    update gpio driver

---
+++ raspi/playground/ldd/src-3/blink.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# path for gpio
+GPIO=/dev/my-gpio-driver
+
+# names of states
+ON="1"
+OFF="0"
+
+ledState()
+{
+	echo $2 > $1
+}
+
+echo "LED is Blinking ... Press 'CNTL+C' to terminate"
+while [ 1 ]
+do
+	ledState $GPIO $ON
+	sleep 1
+
+	ledState $GPIO $OFF
+	sleep 1
+done
---
+++ raspi/playground/ldd/src-3/gpio.mod
@@ -0,0 +1,2 @@
+/home/jura/embedded/raspi/playground/ldd/src-3/gpio.o
+
