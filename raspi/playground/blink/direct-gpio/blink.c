151e2a8faed9d38b94f440a6f4bfebed5d101920
JHlotov <jglotovyn@gmail.com>
2023-04-19 19:43:47 +0100

    minor fixes

--- raspi/playground/blink/direct-gpio/blink.c
+++ raspi/playground/blink/direct-gpio/blink.c
@@ -11,23 +11,21 @@
 
 #define BLOCK_SIZE			(4 * 1024)
 
-int mem_fd;
-void *gpio_map;
-volatile unsigned *gpio;
+static volatile unsigned *gpio;
 
-/**/
-static int init_io(void);
+static int gpioInit(void);
 
 /* GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y) */
 #define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
 #define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
-#define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
-
 #define GPIO_SET *(gpio+7)  /* sets   bits which are 1 ignores bits which are 0 */
 #define GPIO_CLR *(gpio+10) /* clears bits which are 1 ignores bits which are 0 */
  
-int init_io(void)
+int gpioInit(void)
 {
+    int mem_fd;
+    void *gpio_map;
+
     /* open /dev/mem */
     if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0)
 	{
@@ -80,7 +78,7 @@ int main(int argc, char* argv[])
         return -2;
     }
     
-    if (init_io() == -1) 
+    if (gpioInit() == -1) 
     {
         printf("Failed to map the physical GPIO registers into the virtual memory space.\n");
         return -3;
